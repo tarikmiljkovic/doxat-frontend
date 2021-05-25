e97ea8342d1bdce109c9ff3c06ce3f2e1af5c7ad
Miljkovic <tarik.miljkovic@stud.fh-campuswien.ac.at>
2021-05-25 12:36:23 +0200

    wd

--- pages/_document.js
+++ pages/_document.js
@@ -11,13 +11,14 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <title>Doxat</title>
-          <div>
-            <meta
+
+
+             <meta
               name="description"
               content="Doxat Arhitektonski Studio"
             />
 
-            <meta property="og:url" content="https://doxat.ba" />
+ <meta property="og:url" content="https://doxat.ba" />
             <meta
               property="og:site_name"
               content="Doxat Arhitektonski Studio"
@@ -30,7 +31,7 @@ class MyDocument extends Document {
               content="Doxat Arhitektonski Studio"
             />
             <meta name="twitter:title" content="Doxat Arhitektonski Studio" />
-            {/* Social Media */}
+
             <meta property="og:title" content="Doxat Arhitektonski Studio" />
             <meta
               property="og:site_name"
@@ -50,85 +51,31 @@ class MyDocument extends Document {
             />
           </div> */}
 
+
           <link rel="icon" href="favicon.ico" />
-          <div>
-            <link
-              rel="apple-touch-icon"
-              sizes="57x57"
-              href="favicons/apple-icon-57x57.png"
-            />
-            <link
-              rel="apple-touch-icon"
-              sizes="60x60"
-              href="favicons/apple-icon-60x60.png"
-            />
-            <link
-              rel="apple-touch-icon"
-              sizes="72x72"
-              href="favicons/apple-icon-72x72.png"
-            />
-            <link
-              rel="apple-touch-icon"
-              sizes="76x76"
-              href="favicons/apple-icon-76x76.png"
-            />
-            <link
-              rel="apple-touch-icon"
-              sizes="114x114"
-              href="favicons/apple-icon-114x114.png"
-            />
-            <link
-              rel="apple-touch-icon"
-              sizes="120x120"
-              href="favicons/apple-icon-120x120.png"
-            />
-            <link
-              rel="apple-touch-icon"
-              sizes="144x144"
-              href="favicons/apple-icon-144x144.png"
-            />
-            <link
-              rel="apple-touch-icon"
-              sizes="152x152"
-              href="favicons/apple-icon-152x152.png"
-            />
-            <link
-              rel="apple-touch-icon"
-              sizes="180x180"
-              href="favicons/apple-icon-180x180.png"
-            />
-            <link
-              rel="icon"
-              type="image/png"
-              sizes="192x192"
-              href="favicons/android-icon-192x192.png"
-            />
-            <link
-              rel="icon"
-              type="image/png"
-              sizes="32x32"
-              href="favicons/favicon-32x32.png"
-            />
-            <link
-              rel="icon"
-              type="image/png"
-              sizes="96x96"
-              href="favicons/favicon-96x96.png"
-            />
-            <link
-              rel="icon"
-              type="image/png"
-              sizes="16x16"
-              href="favicons/favicon-16x16.png"
-            />
-            <link rel="manifest" href="favicons/manifest.json" />
-            <meta name="msapplication-TileColor" content="#ffffff" />
-            <meta
-              name="msapplication-TileImage"
-              content="favicons/ms-icon-144x144.png"
-            />
-            <meta name="theme-color" content="#ffffff" />
-          </div>
+
+
+          <link rel="apple-touch-icon" sizes="57x57" href="favicons/apple-icon-57x57.png" />
+          <link rel="apple-touch-icon" sizes="60x60" href="favicons/apple-icon-60x60.png" />
+          <link rel="apple-touch-icon" sizes="72x72" href="favicons/apple-icon-72x72.png" />
+          <link rel="apple-touch-icon" sizes="76x76" href="favicons/apple-icon-76x76.png" />
+          <link rel="apple-touch-icon" sizes="114x114" href="favicons/apple-icon-114x114.png" />
+          <link rel="apple-touch-icon" sizes="120x120" href="favicons/apple-icon-120x120.png" />
+          <link rel="apple-touch-icon" sizes="144x144" href="favicons/apple-icon-144x144.png" />
+          <link rel="apple-touch-icon" sizes="152x152" href="favicons/apple-icon-152x152.png" />
+          <link rel="apple-touch-icon" sizes="180x180" href="favicons/apple-icon-180x180.png" />
+          <link rel="icon" type="image/png" sizes="192x192" href="favicons/android-icon-192x192.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="favicons/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="96x96" href="favicons/favicon-96x96.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="favicons/favicon-16x16.png" />
+          <link rel="manifest" href="favicons/manifest.json" />
+          <meta name="msapplication-TileColor" content="#ffffff" />
+          <meta name="msapplication-TileImage" content="favicons/ms-icon-144x144.png" />
+          <meta name="theme-color" content="#ffffff" />
+
+
+
+
         </Head>
         <body>
           <Main />
